94ef0b78c11bafa3cd1e3ef44db0b1b23ef0911d
BATspock <30798316+BATspock@users.noreply.github.com>
2018-01-01 11:06:30 +0530

    new SUAS.py
    cropping of contours and minor improvements in the code

--- SUAS.py
+++ SUAS.py
@@ -5,7 +5,7 @@ import cv2
 
 #PART 1---------------------------------------------------------------
 #resize the image   
-image = cv2.imread('im5.jpg')
+image = cv2.imread('im14.jpg')
 r= 750.0/image.shape[1]
 dim = (750, int(image.shape[0]*r))
 img = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
@@ -35,22 +35,44 @@ res2 = res.reshape((img.shape))
 
 #rebel mode OFF
 
-#PART 3
+#PART 3------------------------------------------------------------------------
 #find relevant contours in the image 
 
 ret, thresh = cv2.threshold(res2, 180, 255, 0)
-edged = cv2.Canny(thresh, 30, 200)
-#image, contours, hirerachy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+img0 = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY) #as findCountours works only on unit8 single channel
+_, contours, _ = cv2.findContours(img0.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+cv2.drawContours(img, contours, -1, (255,0,0), 2)
+
+'''
+print(len(contours))
+for (i, c) in enumerate(contours):
+    print("\tSize of contour %d: %d" % (i, len(c)))
+
+'''
+mask = np.zeros(img.shape, dtype = "uint8")
+
+for c in contours:
+    (x, y, w, h) = cv2.boundingRect(c)
+    cv2.rectangle(mask, (x, y), (x + w, y+ h), (255, 255, 255), -1)
+
+img1 = cv2.bitwise_and(img, mask)
+
+cv2.imshow("mask", img1)
+
+
+
+
+
 
 
 #show images from each part-------------------------------------------
 
 #cv2.imshow('original', image)                                 #part 1
-cv2.imshow('resized', img)                                     #part 1
+#cv2.imshow('resized', img)                                     #part 1
                                   
-cv2.imshow('res', res2)                                        #part 2
+#cv2.imshow('res', res2)                                        #part 2
 
-cv2.imshow('thresh', thresh)                                  #part 3
-cv2.imshow('edged' ,edged)                                      #part 3
+#cv2.imshow('thresh', thresh)                                  #part 3
+#cv2.imshow('gray', img0)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
