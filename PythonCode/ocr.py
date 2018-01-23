828101ae592dca8366c2b7ad9007f00a8ec3a8cd
askarthikkumar <askarthikkumar@gmail.com>
2018-01-23 19:31:11 +0530

    random chnges to ocr.py

--- PythonCode/ocr.py
+++ PythonCode/ocr.py
@@ -1,10 +1,16 @@
 from PIL import Image
 import cv2
 import pytesseract as tsrct
-# add file path
-filePath = "/Users/Starck/Jatayu-IP/PythonCode/crop3.jpeg"
-image = cv2.imread(filePath)
+from Preprocess import Preprocessing
 
+# add file path
+filePath = "/Users/Starck/Jatayu-IP/PythonCode/crop1.jpeg"
+image = cv2.imread(filePath)
+'''
+ip = Preprocessing(image)
+kMeansImg = ip.kmeans(2, image)
+cv2.imshow("kMeansImg", kMeansImg)
+'''
 # convert to gray and threshold
 greyImg = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 threshImg = cv2.threshold(greyImg, 0, 255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)[1]
@@ -16,9 +22,16 @@ cv2.imshow("ThresholdedImage", threshImg)
 threshFile = "tester.png"
 cv2.imwrite(threshFile, threshImg)
 
+ip = Preprocessing(cv2.imread(threshFile))
+blurImg = ip.GaussinaBlur()
+cv2.imshow("BlurredImage", blurImg)
+cv2.imwrite("blurTester.png", blurImg)
 #run through ocr engine
-text = tsrct.image_to_string(Image.open(threshFile))
+text = tsrct.image_to_string(Image.open("blurTester.png"), config='-psm 10000')
 print(text)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 
